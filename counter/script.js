f6875b789875644df567cdb963269fa68d2c4073
aklademarco <157044008+aklademarco@users.noreply.github.com>
2024-06-22 22:17:37 +0000

    Add files via upload

---
+++ counter/script.js
@@ -0,0 +1,21 @@
+
+const decreaseBtn = document.getElementBYId("decreasetn")
+const resetBtn = document.getElementBYId("resetBtn")
+const increaseBtn = document.getElementBYId("increaseBtn")
+const countLabel = document.getElementBYId("countLabel")
+let count = 0;
+
+increaseBtn.onclick = () =>{
+  count++;
+  countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = () =>{
+  count--;
+  countLabel.textContent = count;
+}
+
+resetBtn.onclick = () =>{
+  count = 0;
+  countLabel.textContent = count;
+}
